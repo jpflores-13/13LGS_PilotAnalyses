61f5caac6c9def2b0fcd14d8db7bd13a2fd324d7
JP Flores <jpflores013@gmail.com>
2026-05-08 18:29:17 -0400

    allocate more time on `submit_markers.sh`

--- scripts/slurm/submit_markers.sh
+++ scripts/slurm/submit_markers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=marker_genes
-#SBATCH --time=06:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=8
 #SBATCH --output=logs/marker_genes_%j.out
